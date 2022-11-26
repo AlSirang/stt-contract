80366310f4e1b57abd41e1f8257be4f18dfa6ca9
AlSirang <uddinbashir1999@gmail.com>
2022-11-26 15:44:59 +0500

    feat: add configs

---
+++ README.md
@@ -0,0 +1,17 @@
+# ERC721 Collection
+
+## Commands available to run
+
+You can run the following commands in the root directory.
+
+### yarn install or yarn
+
+Run `yarn install` or `yarn` to install the dependencies.
+
+### npx hardhat compile
+
+Run `npx hardhat compile` to create artifacts.
+
+### npx hardhat test
+
+Run `npx hardhat test` to execute tests scripts.
---
+++ hardhat.config.ts
@@ -0,0 +1,37 @@
+import * as dotenv from "dotenv";
+import { HardhatUserConfig } from "hardhat/config";
+
+import "@openzeppelin/hardhat-upgrades";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.10",
+  paths: { tests: "tests" },
+
+  networks: {
+    ropsten: {
+      url: process.env.ROPSTEN_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
+
+export default config;
