c6eae84f70fdd4a04f5a510198191acb728fb78f
Bashir Uddin <uddinbashir1999@gmail.com>
2023-04-09 22:23:55 +0500

    fix: update merkel root

--- scripts/getMerkelRoot.ts
+++ scripts/getMerkelRoot.ts
@@ -1,11 +1,8 @@
+// @ts-ignore
+import address from "./address.json";
+
 import { getMerkleTreeRootHex } from "./merkel";
 
-const addresses = [
-  "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
-  "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
-  "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC",
-];
-
-const root = getMerkleTreeRootHex(addresses);
+const root = getMerkleTreeRootHex(address);
 
 console.log(root);
